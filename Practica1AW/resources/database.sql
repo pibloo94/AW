747de52e5dcc60ea3a663739e477137a5f63c998
Dani <epocopa@gmail.com>
2019-10-30 11:59:55 +0100

    Update db

--- Practica1AW/resources/database.sql
+++ Practica1AW/resources/database.sql
@@ -14,27 +14,52 @@ CREATE TABLE user(
 	fullname VARCHAR(40),
 	sex CHAR(1),
 	birthdate DATE,
-	image VARCHAR(20)
+	image VARCHAR(20),
+	points INT DEFAULT 0
 );
 
 CREATE TABLE question(
 	id_question INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	user INT NOT NULL,
-	FOREIGN KEY (user) REFERENCES user(id_user)
-
+	title VARCHAR(50),
+	opA VARCHAR(50),
+	opB VARCHAR(50),
+	opC VARCHAR(50),
+	opD VARCHAR(50)
 );
 
 CREATE TABLE answer(
-	id_question INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	question INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	user INT NOT NULL,
-	right BOOLEAN NOT NULL DEFAULT 0,
-	FOREIGN KEY (user) REFERENCES user(id_user)
-
+	answer ENUM('a', 'b', 'c', 'd', 'o'),
+	other VARCHAR(50),
+	FOREIGN KEY (user) REFERENCES user(id_user),
+	FOREIGN KEY (question) REFERENCES question(id_question)
 );
+
+CREATE TABLE answerForOther(
+	question INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	user INT NOT NULL,
+	userGuess INT NOT NULL,
+	answer ENUM('a', 'b', 'c', 'd'),
+	correct BOOLEAN NOT NULL DEFAULT 0,
+	FOREIGN KEY (user) REFERENCES user(id_user),	
+	FOREIGN KEY (userGuess) REFERENCES user(id_user),	
+	FOREIGN KEY (question) REFERENCES question(id_question)
+);
+
 
 CREATE TABLE friend(
 	usera INT NOT NULL,
 	userb INT NOT NULL,
-	FOREIGN KEY (usera, userb) REFERENCES user(id_user),
-	PRIMARY KEY(uasera, userb)
+	FOREIGN KEY (usera) REFERENCES user(id_user),
+	FOREIGN KEY (userb) REFERENCES user(id_user),
+	PRIMARY KEY(usera, userb)
+);
+
+CREATE TABLE request(
+	fromUser INT NOT NULL,
+	toUser INT NOT NULL,
+	FOREIGN KEY (fromUser) REFERENCES user(id_user),
+	FOREIGN KEY (toUser) REFERENCES user(id_user),
+	PRIMARY KEY(fromUser, toUser)
 );
