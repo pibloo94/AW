6178b482832037882d359331290f0eba1d0ba78b
Pablo Agudo Brun <32678514+pibloo94@users.noreply.github.com>
2019-11-11 19:54:39 +0100

    Delete ejnode_test.js

--- Ejercicios/node/ejercicio01/ejnode_test.js
+++
@@ -1,9 +0,0 @@
-const freeplace = require("freeplace");
-
-freeplace.freeplace("fichero1.txt", /[0-9]+/g, {numero}, function(error){
-    if(error){
-        console.log("Error");
-    }else{
-        console.log("Exito");
-    }
-});
